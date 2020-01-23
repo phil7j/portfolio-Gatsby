c2d24d4f69f40e794fd9882a9d2a2a6e56cbfaf6
Philip Johnson <phil7j@gmail.com>
2020-01-23 08:39:28 -0800

    added about page, wip

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -1,42 +1,119 @@
 import React from "react"
 import styled from "styled-components"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
-import Image from "../components/image"
+import Img from "gatsby-image"
 import SEO from "../components/seo"
+import { useStaticQuery, graphql } from "gatsby"
+import {
+  ReactJs,
+  Sass,
+  Javascript,
+  NodeDotJs,
+  Postgresql,
+  Python,
+  Git,
+  Github,
+  Redux,
+} from "@icons-pack/react-simple-icons"
 
-const SecondPage = () => (
-  <>
-    <SEO title="Projects" />
-    <h1 style={{textAlign: "center", marginTop: "80px"}}>Coming Soon...</h1>
-    <AniLink cover duration={0.7} bg="#2d2d2d" direction="down" style={{display: "block", textAlign: "center", marginTop: "80px"}} to="/">Go back to the homepage</AniLink>
-  </>
-//  <>
-//     <SEO title="About" />
-//     <Main>
-//       <Phil>
-//         <Image />
-//       </Phil>
-//       <Headline>
-//         <h1>Philip Johnson</h1>
-//         <h3>Software Engineer</h3>
-//         <AniLink cover duration={0.7} bg="#2d2d2d" direction="down" to="/about">
-//           <button>
-//             <p>Learn More</p>
+const SecondPage = () => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      file(relativePath: { eq: "hiking.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <>
+      <SEO title="About" />
+      <Main>
+        <Skills>
+          <Description>
+            <p>
+              Software Engineer, traveler, soccer aficionado, coffee junkie,
+              family man, with a love for learning new tech and skills.
+            </p>
+            {/* <p></p> */}
+          </Description>
+          <Tech>
+            <div className="tech-section">
+              <h4>Frontend</h4>
+              <TechList>
+              <div class="tech-item">
+                <ReactJs color="#61DAFB" size={28} />
+                <p>React</p>
+              </div>
+              <div class="tech-item">
+                <Redux color="#764ABC" size={28} />
+                <p>Redux</p>
+              </div>
+              <div class="tech-item">
+                <Sass color="#CC6699" size={28} />
+                <p>SASS</p>
+              </div>
+              <div class="tech-item">
+                <Javascript color="#F7DF1E" size={28} />
+                <p>Javascript</p>
+              </div>
 
-//           </button>
-//         </AniLink>
-//       </Headline>
-//     </Main>
-//   </>
-)
+              </TechList>
+            </div>
+            <div className="tech-section">
+              <h4>Backend</h4>
+              <TechList>
+              <div class="tech-item">
+                <NodeDotJs color="#339933" size={28} />
+                <p>Node.js</p>
+              </div>
+              <div class="tech-item">
+                <Postgresql color="#336791" size={28} />
+                <p>PostgreSQL</p>
+              </div>
+              <div class="tech-item">
+                <Python color="#3776AB" size={28} />
+                <p>Python</p>
+              </div>
+              </TechList>
+            </div>
 
+            <div className="tech-section">
+              <h4>Collaboration</h4>
+              <TechList>
+              <div class="tech-item">
+                <Git color="#F05032" size={28} />
+                <p>Git</p>
+              </div>
+              <div class="tech-item">
+                <Github color="#181717" size={28} />
+                <p>Github</p>
+              </div>
+              </TechList>
+            </div>
+          </Tech>
+        </Skills>
+        <Phil>
+          <Img
+            fluid={data.file.childImageSharp.fluid}
+            alt="Me hiking through the Swiss Alps"
+          />
+        </Phil>
+      </Main>
+    </>
+  )
+}
 
 const Main = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
   min-height: 100%;
-  margin: 10rem;
+  margin: 7rem;
+  /* border: 1px solid yellowgreen; */
   @media (max-width: 970px) {
     flex-direction: column;
     margin: 0;
@@ -45,17 +122,74 @@ const Main = styled.div`
 `
 
 const Phil = styled.div`
-  max-width: 500px;
-  min-width: 300px;
+  max-width: 600px;
+  min-width: 400px;
   @media (max-width: 600px) {
     min-width: 270px;
+    order: -1;
   }
 `
-
-const Headline = styled.div`
+const Tech = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 40px;
+  /* border: 1px solid blue; */
+  div.tech-section {
+    margin: 5px;
+    width: 45%;
+    /* background: white; */
+    padding: 10px;
+    /* border-radius: 35px; */
+    border: 1px solid white;
+    h4 {
+      text-align: center;
+      /* color: black; */
+    }
+  }
+  @media (max-width: 1200px) {
+    flex-direction: column;
+    div.tech-section {
+      width: 100%;
+
+    }
+  }
+`
+const TechList = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+align-items: center;
+
+ div.tech-item {
+
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   margin: 10px;
+   p {
+    font-family: "Open Sans Regular", sans-serif;
+    margin-bottom: 0;
+    /* color: black; */
+   }
+ }
+ svg {
+   margin: 2px;
+ }
+`
+
+
+const Description = styled.div`
+  /* border: 1px solid green; */
+  p {
+    margin: auto;
+    font-family: "Glacial Regular", sans-serif;
+    max-width: 380px;
+    @media (max-width: 600px) {
+      width: 100%;
+      margin: 5px;
+    }
+  }
   h1 {
     font-family: "Glacial Regular", sans-serif;
     font-size: 51px;
@@ -74,6 +208,13 @@ const Headline = styled.div`
       font-size: 15px;
     }
   }
+`
+const Skills = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* border: 1px solid red; */
+
   a {
     text-decoration: none;
   }
